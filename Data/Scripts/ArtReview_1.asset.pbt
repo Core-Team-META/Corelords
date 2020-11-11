65cf872fea335e6e84ffcb9babef371be78c45f2
Rolok <imrolok@yahoo.com>
2020-11-10 16:46:54 -0800

    level display
    adds display for level on art review

---
+++ Data/Scripts/ArtReview_1.asset.pbt
@@ -0,0 +1,8 @@
+﻿Assets {
+  Id: 9133805877198360861
+  Name: "ArtReview"
+  PlatformAssetType: 3
+  TextAsset {
+  }
+  SerializationVersion: 68
+}
---
+++ Data/Scripts/ArtReview_1.lua
@@ -0,0 +1,12 @@
+﻿
+function OnLevelSelect(level)
+    UI.ShowFlyUpText("Level " .. level , Vector3.New(0,0,300), {duration = 5})
+end
+
+function OnPlayerJoined(player)
+    if player == Game.GetLocalPlayer() then
+        Events.Connect("LevelSelect", OnLevelSelect)
+    end
+end
+
+Game.playerJoinedEvent:Connect(OnPlayerJoined)
